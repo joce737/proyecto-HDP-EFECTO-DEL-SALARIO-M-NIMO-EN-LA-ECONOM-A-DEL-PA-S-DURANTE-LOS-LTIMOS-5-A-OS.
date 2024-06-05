370797c13e3501cb0b499ef93c1ff22f84e0b926
joce737 <145609607+joce737@users.noreply.github.com>
2024-06-05 01:37:25 -0600

    modificasiones

--- archivosjs/parte3.js
+++ archivosjs/parte3.js
@@ -1,4 +1,7 @@
-function descubre() {
-    var container = document.querySelector('.container');
-    container.classList.toggle('button-clicked');
-}
+document.addEventListener("DOMContentLoaded", function() {
+    var descubreButton = document.getElementById('descubreButton');
+    descubreButton.addEventListener('click', function() {
+        var container = document.querySelector('.container');
+        container.classList.toggle('button-clicked');
+    });
+});
