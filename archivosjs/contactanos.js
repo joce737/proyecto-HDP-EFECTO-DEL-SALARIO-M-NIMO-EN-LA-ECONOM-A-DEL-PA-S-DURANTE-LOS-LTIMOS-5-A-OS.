f8d8736e6cb03bc99189a450812dd4ebdf69d0f6
joce737 <145609607+joce737@users.noreply.github.com>
2024-06-05 23:29:06 -0600

    modificasiones

--- archivosjs/contactanos.js
+++ archivosjs/contactanos.js
@@ -1,4 +1,3 @@
-
 function sendMessage() {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
@@ -6,20 +5,44 @@ function sendMessage() {
     const message = document.getElementById('message').value;
 
     if (name && email && message) {
-        // Aquí puedes añadir la lógica para enviar el mensaje a un servidor o procesar la información de otra manera
-        
-        // Limpiar los campos del formulario
-        document.getElementById('name').value = '';
-        document.getElementById('email').value = '';
-        document.getElementById('phone').value = '';
-        document.getElementById('message').value = '';
+        // Crear objeto con los datos del formulario
+        const data = {
+            nombre: name,
+            correo_electronico: email,
+            telefono: phone,
+            comentario: message
+        };
 
-        // Mostrar el mensaje de éxito
-        document.getElementById('success-message').style.display = 'block';
+        // Enviar datos al servidor usando fetch
+        fetch('/enviar-consulta', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.text())
+        .then(data => {
+            // Limpiar los campos del formulario
+            document.getElementById('name').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('phone').value = '';
+            document.getElementById('message').value = '';
+
+            // Mostrar el mensaje de éxito
+            document.getElementById('success-message').style.display = 'block';
+            
+            // Redirigir a la página de éxito después de 2 segundos
+            setTimeout(() => {
+                window.location.href = 'mensaje_exito.html'; // Cambia esta ruta si es necesario
+            }, 2000);
+        })
+        .catch(error => console.error('Error:', error));
     } else {
         alert('Por favor, rellena todos los campos obligatorios.');
     }
 }
+
 function goToInicio() {
-    window.location.href = "../archivoshtml/parte1.html"; // Cambia esta ruta si es necesario
+    window.location.href = "index.html"; // Cambia esta ruta si es necesario
 }
